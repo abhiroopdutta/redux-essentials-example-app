8d7bbc3ca97be4446b9e73df7589f40938443046
abhiroopdutta <abhiroopd@ymail.com>
2023-01-07 16:22:01 +0000

    Added emoji reactions

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import { PostAuthor } from './PostAuthor';
+import { ReactionButtons } from './ReactionButton';
+import { TimeAgo } from './TimeAgo';
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
 
@@ -19,9 +21,14 @@ export const SinglePostPage = ({ match }) => {
         <section>
             <article className='post'>
                 <h2>{post.title}</h2>
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
+                </div>
                 <p className='post-content'>
                     {post.content}
                 </p>
+                <ReactionButtons post={post} />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
                 </Link>
